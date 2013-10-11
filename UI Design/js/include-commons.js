7e6978668ea422d523dc0a772a9392850d9b419b
VictorG <gsvicky@gmail.com>
2013-10-10 22:15:50 -0700

    modularized the files

---
+++ UI Design/js/include-commons.js
@@ -0,0 +1,25 @@
+// Common Styles
+$('head').append( $('<link rel="stylesheet" type="text/css" media="screen" />')
+	.attr('href', 'css/commonStyle.css') );
+$('head').append( $('<link rel="stylesheet" type="text/css" media="print" />')
+	.attr('href', 'css/print.css') );
+
+// Common Scripts
+$('head').append( $('<script type="text/javascript" charset="utf-8" />')
+	.attr('src', 'js/floating-menu.js') );
+$('head').append( $('<script type="text/javascript" charset="utf-8" />')
+	.attr('src', 'js/zebra-table.js') );
+$('head').append( $('<script type="text/javascript" charset="utf-8" />')
+	.attr('src', 'js/tooltip-link.js') );
+
+// Common Fonts
+$('head').append( $('<link rel="stylesheet" type="text/css" />')
+	.attr('href', 'http://fonts.googleapis.com/css?family=Happy+Monkey') );
+
+// Common Segments
+$(function() {
+	$("#headerbar").load("header.htm"); 
+	$("#menu").load("menu.htm"); 
+	$("#footersection").load("footer.htm"); 
+});
+
